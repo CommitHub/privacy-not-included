2ee20136799a8b3bd945c1ab4a0bd22925e8d4c6
Luis <luis.alvarez@commithub.com>
2020-05-25 12:28:57 -0400

    Added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="footer">
+    <p>
+      Made with 💖 by Luis Alvarez
+      <a href="https://luis-alvarez.dev" target="_blank" rel="noopener">
+        (Portfolio)
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+
+<style lang="scss">
+#footer {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
