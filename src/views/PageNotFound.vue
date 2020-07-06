74107b1a1545d2a4d23f3c1e610524b0629d3088
Luis Alvarez <luis.alvarez@commithub.com>
2020-07-05 20:55:37 -0700

    styled and added 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="page-not-found">
+    <img src="@/assets/images/404.gif" alt="ufo taking notebook" />
+    <div class="info-container">
+      <h1>404</h1>
+      <h2 class="subtext">Oops! The page you are looking for doesn't exist</h2>
+      <button class="btn-main"><router-link to="/">Go Home</router-link></button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../styles/variables.scss";
+
+#page-not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem 0;
+
+  img {
+    width: 30%;
+
+    @media (max-width: $laptop-lg) {
+      width: 60%;
+    }
+
+    @media (max-width: $tablet-lg) {
+      width: 90%;
+    }
+  }
+
+  .info-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    padding: 1rem;
+    margin: 4rem 0;
+
+    .subtext {
+      margin: 2rem 0;
+    }
+  }
+}
+</style>
